3c206192a8887b53d5b69fccc1219cd8be62c686
wdocker <wdocker@wdocker.com>
2023-01-29 01:14:29 -0300

    upload route

--- routes/web.php
+++ routes/web.php
@@ -30,11 +30,10 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified', ])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
     Route::resource('blog', BlogController::class);
+    Route::post('blog/upload', [BlogController::class, 'upload'])->name('blog.upload');
 });
-
-
