1609be4b88afe617dcd4d7145285969d54d374b2
wdocker <wdocker@wdocker.com>
2023-02-25 22:48:01 -0300

    enabled csrf token

--- resources/js/Pages/Blog/Create.vue
+++ resources/js/Pages/Blog/Create.vue
@@ -114,10 +114,10 @@ export default {
                 ckfinder: {
                     // uploadUrl: '/image-upload?_token='+$("input[name='_token']").val(),
                     // uploadUrl: '/image-upload?_token='document.head.querySelector('meta[name="csrf-token"]').content,  TRY WITH THIS
-                    uploadUrl: '/blog/upload?blog=' + this.blog.id,
-                    headers: {
-                        'X-CSRF-TOKEN': document.head.querySelector('meta[name="csrf-token"]').content
-                    }
+                    uploadUrl: '/blog/upload?blog=' + this.blog.id + '&_token=' + document.querySelector("input[name='_token']").value,
+ /*                    headers: {
+                        'X-CSRF-TOKEN': document.querySelector("input[name='_token']").value
+                    } */
                 },
             },
         };
